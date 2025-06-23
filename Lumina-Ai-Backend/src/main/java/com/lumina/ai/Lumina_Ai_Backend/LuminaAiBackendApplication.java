1882f9945bd4ca26f17907b4a9fd8e9369ba0895
sachin <sachinholla01@gmail.com>
2025-06-23 15:48:35 +0530

    Main application added

---
+++ Lumina-Ai-Backend/src/main/java/com/lumina/ai/Lumina_Ai_Backend/LuminaAiBackendApplication.java
@@ -0,0 +1,12 @@
+package com.lumina.ai.Lumina_Ai_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminaAiBackendApplication.class, args);
+    }
+}
